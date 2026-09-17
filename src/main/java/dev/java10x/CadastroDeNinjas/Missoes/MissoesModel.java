b18edcc44f9b5e40ee164260306865509df017e9
moisespds <m.pradodasilva@gmail.com>
2026-09-17 00:12:30 -0300

    build: Conecta o banco H2 a minha aplicacao SPRING

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -20,7 +20,7 @@ public class MissoesModel{
     private Long id;
     private String nome;
     private String dificuldade;
-    private NinjaModel ninja;
+
     //@OneToMany Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
